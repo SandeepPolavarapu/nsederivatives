1b8bad250e438edcc179c3785863d01c41a80972
Sandeep Polavarapu <yesitssandeep@gmail.com>
2013-08-05 21:02:41 +0530

    Added procfile and package

--- server.js
+++ server.js
@@ -1,12 +1,73 @@
-var http = require("http");
+var express = require('express');
 
-function onRequest(request, response) {
-  console.log("Request received.");
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
+var app = express.createServer(express.logger());
+
+// Only possible part that needs to be executed
+app.get('/', function(request, response) {
+	if(request.query.symbol && request.query.symbol.length > 0) {
+		console.log("Symbol Requested - " + request.query.symbol);
+		
+		var http = require('http');
+
+		var symbol = request.query.symbol;
+		var options = {
+		  host: 'www.nseindia.com',
+		  method: 'GET',
+		  path: '/live_market/dynaContent/live_watch/get_quote/getFOHistoricalData.jsp?underlying=' + symbol +'instrument=FUTSTK&expiry=29AUG2013&type=-&strike=-&fromDate=&toDate=&datePeriod=2weeks',
+		  headers: {
+		  	"User-Agent": "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)",
+		  	"Referer": "http://www.nseindia.com/",
+		  	"Accept": '*/*'
+		  }
+		};
+
+		var resp = "";	// Hold the servers response
+
+		var req = http.request(options, function(res) {
+			res.on('error', function(e) {
+				console.log("Got error: " + e.message);
+			});
+			res.on('data', function(data) {
+				resp += data;
+			});
+			res.on('end', function() {
+				resp = resp.trim();
+				data = JSON.parse(resp);
+
+				if(data.data.length < 1) {
+					// Invalid Symbol
+					response.send('<h1>Invalid Symbol</h1>');
+					return;
+				}
+
+				// Cleaning up the feeds
+				delete data.otherSeries;
+				delete data.optLink;
+				delete data.futLink;
+				
+				// Now send the data
+				response.send(data);
+				
+				// @TODO Can still filter to provide more precise data 
+			});
+		});
+		req.end();
+	} else {
+		// Invalid request
+		response.send(invalidRequest(), 400);
+	}
+});
+
+// Map all other request
+app.get('/**', function(req, res) {
+	res.send(invalidRequest(), 400);
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
+function invalidRequest() {
+	return "<h1>INVALID REQUEST</h1><p>Request must be of the form:\r\n <strong>http://nsederivatives.herokuapp.com/?symbol=INFY</strong></p>\r\n";
 }
-
-http.createServer(onRequest).listen(8888);
-
-console.log("Server has started.");
